663860306cd02803b6ab95245532fa7f45088442
ediaz13 <diazemanuel27@gmail.com>
2024-11-01 17:03:22 -0300

    27. Topic Exchange Configuration with Spring AMQP

--- src/main/java/com/listener/listeners/RabbitMQExchangeConfiguration.java
+++ src/main/java/com/listener/listeners/RabbitMQExchangeConfiguration.java
@@ -21,4 +21,13 @@ public class RabbitMQExchangeConfiguration {
                 .internal()
                 .build();
     }
+
+    @Bean
+    Exchange newExchange() {
+        return ExchangeBuilder.topicExchange("TopicTestExchange")
+                .autoDelete()
+                .internal()
+                .durable(true)
+                .build();
+    }
 }
