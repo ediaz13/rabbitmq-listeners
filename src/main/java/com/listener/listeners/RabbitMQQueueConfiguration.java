3a2698e6834c5befae716a39d029f31e75dd397f
ediaz13 <diazemanuel27@gmail.com>
2024-11-01 17:24:28 -0300

    29. Headers Exchange Configuration with RabbitMQ

--- src/main/java/com/listener/listeners/RabbitMQQueueConfiguration.java
+++ src/main/java/com/listener/listeners/RabbitMQQueueConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.amqp.core.QueueBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+//HA
 @Configuration
 public class RabbitMQQueueConfiguration {
     @Bean
