0586f09d2015a6ee547a547adf893a54a484b15f
ediaz13 <diazemanuel27@gmail.com>
2024-11-01 19:43:04 -0300

    30. Creating Bindings Between Queues and Exchanges

--- src/main/java/com/listener/listeners/RabbitMQConfig.java
+++ src/main/java/com/listener/listeners/RabbitMQConfig.java
@@ -1,8 +1,8 @@
 package com.listener.listeners;
 
 //import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.listener.MessageListenerContainer;
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
@@ -19,6 +19,24 @@ public class RabbitMQConfig {
     @Bean
     public Queue myQueue() {
         return new Queue(MY_QUEUE, true);
+    }
+
+    @Bean
+    Exchange myExchange() {
+        return ExchangeBuilder.topicExchange("MyTopicExchange")
+                .durable(true)
+                .build();
+    }
+
+    @Bean
+    Binding binding() {
+        //return new Binding(MY_QUEUE, Binding.DestinationType.QUEUE, "MyTopicExchange", "topic", null);
+        return BindingBuilder
+                .bind(myQueue())
+                .to(myExchange())
+                .with("topic")
+                .noargs();
+
     }
 
     @Bean
